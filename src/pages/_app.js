91c9837afb2f634a311b1c99564da6487ffab312
Karthik <karthik.nishanth06@gmail.com>
2023-03-29 12:12:19 +0100

    gregehg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,36 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
-    <SessionProvider>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-LSLF7F9MS0"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-LSLF7F9MS0', {page_path: window.location.pathname,});`,
+        }}
+      />
+      <SessionProvider>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
