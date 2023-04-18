d99199ccd8829e4b394f926906fb8c5e7433b213
Karthik <karthik.nishanth06@gmail.com>
2023-04-18 23:09:15 +0100

    ferger

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,8 +1,3 @@
-import { useState, useEffect } from "react";
-import Head from "next/head";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { generateBG } from "../utils/bgAnim.js";
 import {
   FaCss3Alt,
   FaHtml5,
@@ -20,401 +15,62 @@ import {
   SiMysql,
   SiFigma,
 } from "react-icons/si";
+import { motion } from "framer-motion";
+const Skill = ({ title, Icon, color }) => (
+  <div className="flex flex-col items-center">
+    <p className="mb-2 text-lg font-bold">{title}</p>
+    <Icon className="text-5xl" style={{ color }} />
+  </div>
+);
+
 function Skills() {
-  const router = useRouter();
+  const programmingSkills = [
+    { title: "HTML", Icon: FaHtml5, color: "#DD4B25" },
+    { title: "CSS", Icon: FaCss3Alt, color: "#3594CF" },
+    { title: "React", Icon: FaReact, color: "#5BD3F3" },
+    { title: "Javascript", Icon: DiJavascript1, color: "#EFD81D" },
+    { title: "NodeJs", Icon: FaNode, color: "#5B9853" },
+    { title: "Bootstrap", Icon: FaBootstrap, color: "#700EED" },
+    { title: "TailwindCSS", Icon: SiTailwindcss, color: "#700EED" },
+    { title: "MySQL", Icon: SiMysql, color: "#005E86" },
+  ];
 
-  let { comeThru } = router.query;
-  const [elemProps, setElemProps] = useState({});
+  const designTools = [
+    { title: "Photoshop", Icon: SiAdobephotoshop, color: "#2FA3F7" },
+    { title: "Illustrator", Icon: SiAdobeillustrator, color: "#F79500" },
+    { title: "Adobe XD", Icon: SiAdobexd, color: "#F75EEE" },
+    { title: "DreamWeaver", Icon: SiAdobedreamweaver, color: "#5B9853" },
+    { title: "Figma", Icon: SiFigma, color: "#F76D60" },
+  ];
 
-  useEffect(() => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const initialElemProps = generateBG(width, height, false);
-    setElemProps(initialElemProps);
-
-    let resized = false;
-
-    const handleResize = () => {
-      if (!resized) {
-        resized = true;
-
-        if (width !== window.innerWidth || height !== window.innerHeight) {
-          const newElemProps = generateBG(
-            window.innerWidth,
-            window.innerHeight,
-            true
-          );
-          setElemProps(newElemProps);
-        }
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
   return (
-    <>
-      <div className="wrapper no-bg">
-        <Head>
-          <title>skills // karthik nishanth.</title>
-          <meta
-            name="description"
-            content="A creative designer and developer based in London."
-          />
-        </Head>
+    <div className="min-h-screen p-8">
+      <h2 className="mb-6 text-white text-3xl font-bold">
+        Programming languages
+      </h2>
+      <motion.div
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.1 }}
+        className="grid grid-cols-4 bg-gradient-to-t from-gray-800 to-black text-white p-4 gap-8"
+      >
+        {programmingSkills.map((skill) => (
+          <Skill key={skill.title} {...skill} />
+        ))}
+      </motion.div>
 
-        <header>
-          <Link
-            href={{
-              pathname: "/",
-              query: { comeThru: comeThru ? "true" : undefined },
-            }}
-          >
-            <h1>karthik nishanth.</h1>
-            <span></span>
-          </Link>
-          <div>
-            <span></span>
-            <span className="alt"></span>
-          </div>
-        </header>
-
-        <div className="contact site-cont">
-          <div className="meta">
-            <ul>
-              <li>
-                <h2>Programming languages</h2>
-                <div className="prog">
-                  <div
-                    className="proglogo"
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      HTML
-                    </ol>
-                    <FaHtml5 style={{ fontSize: "60px", color: "#DD4B25" }} />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      CSS
-                    </ol>
-                    <FaCss3Alt style={{ fontSize: "60px", color: "#3594CF" }} />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      React
-                    </ol>
-                    <FaReact style={{ fontSize: "60px", color: "#5BD3F3" }} />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Javascript
-                    </ol>
-                    <DiJavascript1
-                      style={{
-                        fontSize: "80px",
-                        color: "#EFD81D",
-                        marginLeft: "10px",
-                        marginTop: "-10px",
-                      }}
-                    />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      NodeJs
-                    </ol>
-                    <FaNode
-                      style={{
-                        fontSize: "80px",
-                        color: "#5B9853",
-                        marginLeft: "10px",
-                      }}
-                    />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Bootstrap
-                    </ol>
-                    <FaBootstrap
-                      style={{
-                        fontSize: "70px",
-                        color: "#700EED",
-                        marginLeft: "10px",
-                      }}
-                    />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      marginLeft: "10px",
-                    }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      TailwindCSS
-                    </ol>
-                    <SiTailwindcss
-                      style={{
-                        fontSize: "80px",
-                        color: "#700EED",
-                        marginLeft: "10px",
-                      }}
-                    />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      marginLeft: "10px",
-                    }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      MySql
-                    </ol>
-                    <SiMysql
-                      style={{
-                        fontSize: "80px",
-                        color: "#005E86",
-                        marginLeft: "10px",
-                      }}
-                    />
-                  </div>
-                </div>
-                {/* <img
-              width="70px"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/363px-CSS3_logo_and_wordmark.svg.png"
-            /> */}
-              </li>
-              <li>
-                <h2>Design Tools</h2>
-                <div className="prog">
-                  <div
-                    className="proglogo"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      marginRight: "10px",
-                      alignItems: "center",
-                    }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Photoshop
-                    </ol>
-                    <SiAdobephotoshop
-                      style={{ fontSize: "60px", color: "#2FA3F7" }}
-                    />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      marginRight: "10px",
-                      alignItems: "center",
-                    }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Illustrator
-                    </ol>
-                    <SiAdobeillustrator
-                      style={{
-                        fontSize: "60px",
-                        color: "#F79500",
-                      }}
-                    />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      marginRight: "10px",
-                      alignItems: "center",
-                    }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      AdobeXd
-                    </ol>
-                    <SiAdobexd style={{ fontSize: "60px", color: "#F75EEE" }} />
-                  </div>
-
-                  <div
-                    className="proglogo"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "center",
-                      marginRight: "10px",
-                    }}
-                  >
-                    <ol
-                      style={{
-                        margin: "0px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      DreamWeaver
-                    </ol>
-                    <SiAdobedreamweaver
-                      style={{
-                        fontSize: "60px",
-                        color: "#5B9853",
-                      }}
-                    />
-                  </div>
-                  <div
-                    className="proglogo"
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <ol
-                      style={{
-                        marginLeft: "-10px",
-                        padding: "0px",
-                        fontSize: "20px",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      Figma
-                    </ol>
-                    <SiFigma
-                      style={{
-                        fontSize: "70px",
-                        color: "#F76D60",
-                      }}
-                    />
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div
-          className={"background-overlay anim "}
-          style={{
-            gridTemplateColumns: "repeat(" + elemProps.gridSizeX + ", 1fr",
-          }}
-        >
-          {[
-            ...Array((elemProps.gridSizeX || 0) * (elemProps.gridSizeY || 0)),
-          ].map((e, i) => (
-            <span key={i}></span>
-          ))}
-        </div>
-      </div>
-    </>
+      <h2 className="mt-12 text-white mb-6 text-3xl font-bold">Design Tools</h2>
+      <motion.div
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.3 }}
+        className="grid grid-cols-5 bg-gradient-to-b from-gray-800 to-black text-white p-4 gap-8"
+      >
+        {designTools.map((tool) => (
+          <Skill key={tool.title} {...tool} />
+        ))}
+      </motion.div>
+    </div>
   );
 }
 
