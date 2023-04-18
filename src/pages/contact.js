784266d4e6488ff26a36fa03786b8d0a62d8bb5c
Karthik <karthik.nishanth06@gmail.com>
2023-04-18 23:35:09 +0100

    fwff

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,121 +1,60 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { generateBG } from "../utils/bgAnim.js";
+
 import Head from "next/head";
 function Contact() {
-  useEffect(() => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const initialElemProps = generateBG(width, height, false);
-    setElemProps(initialElemProps);
-
-    let resized = false;
-
-    const handleResize = () => {
-      if (!resized) {
-        resized = true;
-
-        if (width !== window.innerWidth || height !== window.innerHeight) {
-          const newElemProps = generateBG(
-            window.innerWidth,
-            window.innerHeight,
-            true
-          );
-          setElemProps(newElemProps);
-        }
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    // Clean up the event listener when the component is unmounted
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-  const router = useRouter();
-  const [elemProps, setElemProps] = useState({});
   return (
     <>
       <Head>
-        {" "}
         <title>contact // karthik nishanth.</title>
       </Head>
-      <div className="wrapper no-bg">
-        <header>
-          <Link href="/">
-            <h1>karthik nishanth.</h1>
-            <span></span>
-          </Link>
-          <div>
-            <span></span>
-            <span className="alt"></span>
+      <div className="min-h-screen p-4 md:p-8 flex flex-col font-display text-white">
+        <h2 className="mb-6 text-3xl font-bold mx-auto">Get in touch</h2>
+        <div className="max-w-5xl mx-auto">
+          <div className="mb-8">
+            <p className="text-lg">
+              From a passionate production undergraduate with a keen interest in
+              exploring cutting-edge technologies and building visually
+              appealing software solutions. As a quick learner with excellent
+              problem-solving and teamwork abilities, I enjoy designing and
+              creating intuitive user interfaces and products that streamline
+              workflows for not only myself but thousands of developers
+              worldwide. Check out some of my achievements below.
+            </p>
           </div>
-        </header>
+          <ul className="list-disc pl-6 mb-8">
+            <li className="mb-2">
+              Finalist in Bizzventure, an annual pitching competition for our
+              business idea 'PABO' - an innovative approach to tackling parking
+              issues in bustling cities.
+            </li>
+            <li className="mb-2">
+              Completed an internship at Talent Logistics, Egypt, where I
+              provided them with competitive digital solutions to expand their
+              reach to other countries.
+            </li>
+            <li className="mb-2">
+              Acquired years of experience working with JavaScript (ES6),
+              Python, PHP, HTML, CSS (SASS/SCSS), Node.js, React, GitHub/Git,
+              Firebase, SQL, WordPress, and Adobe XD.
+            </li>
+          </ul>
 
-        <div className="contact site-cont">
-          <div className="meta">
-            <div className="intro">
-              <p>
-                Hey! I am production graduate who loves learning new tech, and
-                building pretty things — mostly software. I am an individual
-                with great problem-solving and team-working skills. And in my
-                free time, I design and build intuitive user-interfaces and make
-                products that not only help my own workflow, but thousands of
-                other developers. Here are a couple of accomplishments.
-              </p>
-            </div>
-            <ul>
-              {/* <li>Achieved 86% in the first year of Computer Science BSc</li> */}
-              <li>
-                Finalist in Bizzventure,a Annual Pitching Competition for our
-                Buisness Idea `PABO`-a innovative solution to parking in busy
-                cities.
-              </li>
-              <li>
-                Interned in Talent Logistics,Egypt - giving them competitive
-                solution to reach out digitally so to improve their boundries to
-                other countries.
-              </li>
-              <li>
-                Years of experience working with JavaScript (ES6), Python, PHP,
-                HTML, CSS (SASS/SCSS), Node.js, React, GitHub/Git, Firebase,
-                SQL, WordPress, and Adobe XD.
-              </li>
-              {/* <li>Languages: English, Bengali, Hindi (speaking), Urdu (speaking)</li> */}
-            </ul>
-
-            <div className="socials">
-              {/* <a target="_blank" href={pdf}>
-                Download CV
-              </a> */}
-              <a href="mailto:karthik.nishanth06@gmail.com">Email</a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://github.com/karthiknish"
-              >
-                GitHub
-              </a>
-              <a target="_blank" rel="noopener noreferrer">
-                LinkedIn
-              </a>
-            </div>
+          <div className="flex space-x-4">
+            {/* <a
+              target="_blank"
+              href={pdf}
+              className="px-4 py-2 bg-green-600 hover:bg-green-700 rounded"
+            >
+              Download CV
+            </a> */}
+            <a
+              href="mailto:karthik.nishanth06@gmail.com"
+              className="px-4 py-2 bg-blue-600 hover:bg-blue-700 rounded"
+            >
+              Email ME!
+            </a>
           </div>
-        </div>
-
-        <div
-          className={"background-overlay anim "}
-          style={{
-            gridTemplateColumns: "repeat(" + elemProps.gridSizeX + ", 1fr",
-          }}
-        >
-          {[
-            ...Array((elemProps.gridSizeX || 0) * (elemProps.gridSizeY || 0)),
-          ].map((e, i) => (
-            <span key={i}></span>
-          ))}
         </div>
       </div>
     </>
