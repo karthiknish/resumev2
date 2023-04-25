ccc0c5d0fd513874c872e1285cacb8488c9a9f84
Karthik <karthik.nishanth06@gmail.com>
2023-04-25 17:15:05 +0100

    fefw

--- src/pages/api/generate-cover-letter.js
+++ src/pages/api/generate-cover-letter.js
@@ -2,6 +2,8 @@ const dotenv = require("dotenv");
 import fetch from "node-fetch";
 import { getSession } from "next-auth/react";
 import dbConnect from "../../lib/dbConnect";
+import puppeteer from "puppeteer";
+
 dotenv.config();
 import ResumeExtract from "../../models/ResumeExtract";
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
@@ -9,21 +11,33 @@ const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 const CUSTOM_SEARCH_ENGINE_ID = process.env.CUSTOM_SEARCH_ENGINE_ID;
 const GOOGLE_SEARCH_API_URL = "https://www.googleapis.com/customsearch/v1";
 const OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
-async function fetchJobDescription(companyName, role) {
-  const query = `job description for ${role} at ${companyName} based in UK`;
-  const url = `${GOOGLE_SEARCH_API_URL}?key=${GOOGLE_API_KEY}&cx=${CUSTOM_SEARCH_ENGINE_ID}&q=${encodeURIComponent(
-    query
-  )}`;
+// async function fetchJobDescription(companyName, role) {
+//   const query = `job description for ${role} at ${companyName} based in UK`;
+//   const url = `${GOOGLE_SEARCH_API_URL}?key=${GOOGLE_API_KEY}&cx=${CUSTOM_SEARCH_ENGINE_ID}&q=${encodeURIComponent(
+//     query
+//   )}`;
 
-  const response = await fetch(url);
-  const data = await response.json();
+//   const browser = await puppeteer.launch({ headless: true });
+//   const page = await browser.newPage();
+//   await page.goto(url);
 
-  if (data.items && data.items.length > 0) {
-    console.log(data.items[0].snippet);
-    return data.items[0].snippet;
-  }
-  return "";
-}
+//   // Get the first search result link
+//   const firstLinkSelector = "div.yuRUbf > a";
+//   await page.waitForSelector(firstLinkSelector);
+//   const firstLink = await page.$eval(firstLinkSelector, (el) => el.href);
+
+//   // Go to the first search result page and scrape the job description
+//   await page.goto(firstLink);
+//   const jobDescriptionSelector = "div.job-description"; // Update this selector based on the target website's structure
+//   await page.waitForSelector(jobDescriptionSelector);
+//   const jobDescription = await page.$eval(
+//     jobDescriptionSelector,
+//     (el) => el.innerText
+//   );
+
+//   await browser.close();
+//   return jobDescription;
+// }
 export default async function handler(req, res) {
   await dbConnect();
   const session = await getSession({ req });
@@ -57,14 +71,14 @@ export default async function handler(req, res) {
 
     case "POST":
       const { companyName, role, extractedText } = req.body;
-      let jobDescription;
-      try {
-        jobDescription = await fetchJobDescription(companyName, role);
-      } catch (error) {
-        console.error("Failed to fetch job description:", error);
-        res.status(500).json({ error: "Failed to fetch job description" });
-        return;
-      }
+      // let jobDescription;
+      // try {
+      //   jobDescription = await fetchJobDescription(companyName, role);
+      // } catch (error) {
+      //   console.error("Failed to fetch job description:", error);
+      //   res.status(500).json({ error: "Failed to fetch job description" });
+      //   return;
+      // }
       try {
         await ResumeExtract.findOneAndUpdate(
           { userEmail },
@@ -76,8 +90,8 @@ export default async function handler(req, res) {
         res.status(500).json({ error: "Failed to save extracted text" });
         return;
       }
-      if (jobDescription !== "") {
-        const prompt = `As an applicant who is highly interested in the role of ${role} at ${companyName}, I believe my skills and experience make me a valuable addition to the team. Create a compelling cover letter with atleast 400 words that will convince the hiring manager of my potential based on the following job description:\n${jobDescription}\nExplain the Resume summary:\n${extractedText} in the context of the posting.`;
+      if (role !== "") {
+        const prompt = `As an applicant who is highly interested in the role of ${role} at ${companyName}, I believe my skills and experience make me a valuable addition to the team. Create a compelling cover letter with atleast 400 words that will convince the hiring manager of my potential based on the job description.Explain the Resume summary:\n${extractedText} in the context of the job posting.`;
         try {
           const response = await fetch(OPENAI_API_URL, {
             method: "POST",
