5c5fa4f65e5ec564eb23c960fde3f77032ef474d
Karthik <karthik.nishanth06@gmail.com>
2025-02-08 14:34:17 +0530

    push

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -27,6 +27,22 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap"
           rel="stylesheet"
+        />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16861209706"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-16861209706');
+            `,
+          }}
         />
       </Head>
       <body>
